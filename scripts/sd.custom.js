7e9ca2feaf8264a83babb5800f99baa3360aef4e
net-solution-developer <51338626+net-solution-developer@users.noreply.github.com>
2019-06-03 18:15:41 -0400

    Add files via upload

---
+++ scripts/sd.custom.js
@@ -0,0 +1,15 @@
+﻿if (typeof (SD) != "undefined") {
+    _.cl("sd.custom.js is loaded", "success");
+    SD.prototype.Custom = {
+        Load: () => {
+            var sd = this.parent.SD, utils = sd.Utils, ui = sd.UI, e = sd.Events, project = sd.Project, me = project;
+
+        },
+        _: function () {
+            this.parent = _Window;;
+            delete this._;
+            return this;
+        }
+    }._();
+} else
+    _.cl("You must include sd.core.js before sd.custom.js", "warning")
