a6cc5d46951b02e6a0a172cfd0ce0f1d8dd66262
net-solution-developer <51338626+net-solution-developer@users.noreply.github.com>
2019-06-06 11:41:41 -0400

    Update sd.project.js

--- scripts/sd.project.js
+++ scripts/sd.project.js
@@ -2,7 +2,7 @@ if (typeof (SD) != "undefined") {
     _.cl("sd.project.js is loaded", "success");
     SD.prototype.Project = {
         Root: "/virualdirectory",
-        ServiceUrl: "/services/service.svc",
+        ServiceUrl: "/services/",
         Load: () => {
             var sd = this.parent.SD, utils = sd.Utils, ui = sd.UI, e = sd.Events, project = sd.Project, me = this;
 
@@ -70,11 +70,57 @@ if (typeof (SD) != "undefined") {
                     }]
                 };
                 utils.HTMLForm(config);
-                c.txtConfig.value = JSON.stringify(config);
+                //c.txtCode.value = JSON.stringify(config);
+                var editorConfig = {
+                    lineNumbers: true,
+                    matchBrackets: true,
+                    continueComments: "Enter",
+                    extraKeys: { "Ctrl-Q": "toggleComment" }
+                };
 
+                var editor = CodeMirror.fromTextArea(document.getElementById("txtCode"), editorConfig );
+                CodeMirror.fromTextArea(document.getElementById("txtCode2"), editorConfig);
+                CodeMirror.fromTextArea(document.getElementById("txtCode3"), editorConfig);
+                CodeMirror.fromTextArea(document.getElementById("txtCode4"), editorConfig);
+                
+                
 
                 _.on(c.btnRun, false, e.OnClick, () => {
-                    var config = c.txtConfig.value;
+
+                    var fnRender = (tag, item) => {
+                        var a = _.ce("a");
+                        a.innerHTML = item.Title;
+                        a.href = "javascript:void";
+                        a.onclick = (me) => {
+                            var obj = JSON.parse(me.target.getAttribute("obj"));
+                            txtmTitle.value = obj.Title;
+                            txtmCategoryOriginal.value = obj.Category;
+                            txtmCategory.value = "";
+                            txtmSource.value = obj.Source;
+                            txtmText.value = obj.Text;
+                            txtmReference.value = obj.Link;
+                            txtmImage.value = obj.Image;
+                        }
+                        a.setAttribute("obj", JSON.stringify(item));
+                        var h2 = _.ce("h2");
+                        h2.appendChild(a);
+                        var p = _.ce("p");
+                        p.innerHTML = item.Text;
+                        var span = _.ce("span");
+                        span.innerHTML = item.Category;
+                        var small = _.ce("small");
+                        small.innerHTML = item.Source;
+                        var img = _.ce("img");
+                        img.src = item.Image;
+                        tag.appendChild(h2);
+                        tag.appendChild(img);
+                        tag.appendChild(span);
+                        tag.appendChild(small);
+                        tag.appendChild(p);
+                        tag.setAttribute("data-source", item.Source);
+                    };
+
+                    var config = editor.getValue();
                     utils.HTMLForm(JSON.parse(config));
                 });
                 _.on(c.btnHelp, false, e.OnClick, () => {
